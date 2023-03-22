941db11b352160ff25a4b6e3c1f98b39bb3cbbe2
Rubio <paulocezarrubio@gmail.com>
2023-03-22 16:24:27 -0300

    req4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,43 +1,51 @@
 def is_anagram(first_string, second_string):
-    firstString = str(first_string)
-    secondString = str(second_string)
-    if len(firstString) != len(secondString):
-        return False
-    if (len(firstString) == 0 or len(secondString) == 0):
-        return False
+    firstString = list(first_string.lower())
+    secondString = list(second_string.lower())
 
-    sorted_first = merge_sort(list(firstString))
-    sorted_second = merge_sort(list(secondString))
+    merge_sort(firstString)
+    merge_sort(secondString)
 
-    for i in range(len(sorted_first)):
-        if sorted_first[i] != sorted_second[i]:
-            return False
+    joinFirst = "".join(firstString)
+    joinSecond = "".join(secondString)
 
-    return True
+    if len(joinFirst) != len(joinSecond):
+        return (joinFirst, joinSecond, False)
+    if (len(joinFirst) == 0 or len(joinSecond) == 0):
+        return (joinFirst, joinSecond, False)
 
+    for i in range(len(joinFirst)):
+        if joinFirst[i] != joinSecond[i]:
+            return (joinFirst, joinSecond, False)
 
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst
-
-    mid = len(lst) // 2
-    left = merge_sort(lst[:mid])
-    right = merge_sort(lst[mid:])
-    return merge(left, right)
+    return (joinFirst, joinSecond, True)
 
 
-def merge(left, right):
-    merged = []
-    left_idx, right_idx = 0, 0
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1: # se não reduzi o suficiente, continua
+        mid = (start + end) // 2 # encontrando o meio
+        merge_sort(numbers, start, mid) # dividindo as listas
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end) # unindo as listas
 
-    while left_idx < len(left) and right_idx < len(right):
-        if left[left_idx] <= right[right_idx]:
-            merged.append(left[left_idx])
-            left_idx += 1
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid] # indexando a lista da esquerda
+    right = numbers[mid:end] # indexando a lista da direita
+
+    left_index, right_index = 0, 0 # as duas listas começarão do início
+
+    for general_index in range(start, end): # percorrer sobre a lista inteira como se fosse uma
+        if left_index >= len(left): # se os elementos da esquerda acabaram, preenche o restante com a lista da direita 
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right): # se os elementos da direita acabaram, preenche o restante com a lista da esquerda
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]: # se o elemento do topo da esquerda for menor que o da direita, ele será o escolhido
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
         else:
-            merged.append(right[right_idx])
-            right_idx += 1
-
-    merged.extend(left[left_idx:])
-    merged.extend(right[right_idx:])
-    return merged
+            numbers[general_index] = right[right_index] # caso o da direita seja menor, ele será o escolhido
+            right_index = right_index + 1
