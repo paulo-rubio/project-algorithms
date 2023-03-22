80895cfc407c2f0e967a8f2f4ada89e4f0835394
Rubio <paulocezarrubio@gmail.com>
2023-03-22 14:48:04 -0300

    initial commit

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) != len(second_string):
+        return False
+
+    freq1 = {}
+    freq2 = {}
+    for char in first_string:
+        freq1[char] = freq1.get(char, 0) + 1
+    for char in second_string:
+        freq2[char] = freq2.get(char, 0) + 1
+    
+    # Compara as frequências
+    for char in freq1:
+        if char not in freq2 or freq1[char] != freq2[char]:
+            return False
+    
+    return True
