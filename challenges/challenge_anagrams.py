0e178258905dba9307835df4de0413f8839ae69d
Rubio <paulocezarrubio@gmail.com>
2023-03-22 16:31:35 -0300

    fix_req4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -23,29 +23,29 @@ def is_anagram(first_string, second_string):
 def merge_sort(numbers, start=0, end=None):
     if end is None:
         end = len(numbers)
-    if (end - start) > 1: # se não reduzi o suficiente, continua
-        mid = (start + end) // 2 # encontrando o meio
-        merge_sort(numbers, start, mid) # dividindo as listas
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
         merge_sort(numbers, mid, end)
-        merge(numbers, start, mid, end) # unindo as listas
+        merge(numbers, start, mid, end)
 
 
 def merge(numbers, start, mid, end):
-    left = numbers[start:mid] # indexando a lista da esquerda
-    right = numbers[mid:end] # indexando a lista da direita
+    left = numbers[start:mid]
+    right = numbers[mid:end]
 
-    left_index, right_index = 0, 0 # as duas listas começarão do início
+    l_i, r_i = 0, 0
 
-    for general_index in range(start, end): # percorrer sobre a lista inteira como se fosse uma
-        if left_index >= len(left): # se os elementos da esquerda acabaram, preenche o restante com a lista da direita 
-            numbers[general_index] = right[right_index]
-            right_index = right_index + 1
-        elif right_index >= len(right): # se os elementos da direita acabaram, preenche o restante com a lista da esquerda
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
-        elif left[left_index] < right[right_index]: # se o elemento do topo da esquerda for menor que o da direita, ele será o escolhido
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
+    for g_i in range(start, end):
+        if l_i >= len(left):
+            numbers[g_i] = right[r_i]
+            r_i = r_i + 1
+        elif r_i >= len(right):
+            numbers[g_i] = left[l_i]
+            l_i = l_i + 1
+        elif left[l_i] < right[r_i]:
+            numbers[g_i] = left[l_i]
+            l_i = l_i + 1
         else:
-            numbers[general_index] = right[right_index] # caso o da direita seja menor, ele será o escolhido
-            right_index = right_index + 1
+            numbers[g_i] = right[r_i]
+            r_i = r_i + 1
