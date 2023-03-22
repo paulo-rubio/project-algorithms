37e03991574449061db990ead3d2de1b82c94a02
Rubio <paulocezarrubio@gmail.com>
2023-03-22 15:24:15 -0300

    test4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,43 @@
 def is_anagram(first_string, second_string):
-    if len(first_string) != len(second_string):
+    firstString = str(first_string)
+    secondString = str(second_string)
+    if len(firstString) != len(secondString):
+        return False
+    if (len(firstString) == 0 or len(secondString) == 0):
         return False
 
-    freq1 = {}
-    freq2 = {}
-    for char in first_string:
-        freq1[char] = freq1.get(char, 0) + 1
-    for char in second_string:
-        freq2[char] = freq2.get(char, 0) + 1
-    
-    # Compara as frequências
-    for char in freq1:
-        if char not in freq2 or freq1[char] != freq2[char]:
+    sorted_first = merge_sort(list(firstString))
+    sorted_second = merge_sort(list(secondString))
+
+    for i in range(len(sorted_first)):
+        if sorted_first[i] != sorted_second[i]:
             return False
-    
+
     return True
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    return merge(left, right)
+
+
+
+def merge(left, right):
+    merged = []
+    left_idx, right_idx = 0, 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            merged.append(left[left_idx])
+            left_idx += 1
+        else:
+            merged.append(right[right_idx])
+            right_idx += 1
+
+    merged.extend(left[left_idx:])
+    merged.extend(right[right_idx:])
+    return merged
